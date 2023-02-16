494d9433b1166364d61fd99f426feb416f377d8f
Gaming Brat ta ta te <Gaming Brat ta ta te@DESKTOP-GI3PO28>
2023-02-16 13:19:26 +0100

    ...

---
+++ LoveMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCoreKoans
+{
+    internal class LoveMethods
+    {
+        public LoveMethods()
+        {
+            Console.WriteLine("Miss you Brian...");
+        }
+    }
+}
